87e70458e8c0520b28f975609d88f75bd4795b37
Noman Shamim <nomanshamim2004@gmail.com>
2024-06-26 23:59:42 +0500

    server connected from DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Successfully connected to mongoDB.");
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log(`Successfully connected to mongoDB`);
     } catch (error) {
         console.error(`Error-->" , ${error.message}`);
         process.exit(1)
     }
-}
+};
+
+export default connectDB;
