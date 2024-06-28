39786585fd1f4c67940af990b2de5fa1d2454b8d
Noman Shamim <nomanshamim2004@gmail.com>
2024-06-29 00:26:07 +0500

    deleteUser , updateUserProfile work done.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,9 +70,72 @@ const getAllUsers = asyncHandler(async (req, res) => {
     res.json(users);
 })
 
+const getCurrentUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.send({
+            _id: user._id,
+            username: user.username,
+            email: user.email
+        })
+    } else {
+        res.status(404)
+        throw new Error("User not found.")
+    }
+});
+
+const updateCurrentUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.username = req.body.username || user.username;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(req.body.password, salt)
+            user.password = hashedPassword;
+        }
+
+        const updateUser = await user.save();
+
+        res.json({
+            _id: updateUser._id,
+            username: updateUser.username,
+            email: updateUser.email,
+            isAdmin: updateUser.isAdmin
+        })
+    } else {
+        res.status(404)
+        throw new Error("User not found.")
+    }
+});
+
+const deleteUserById = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    if (user) {
+        if (user.isAdmin) {
+            res.status(400);
+            throw new Error("Cannot delete admin user.")
+        }
+
+        await User.deleteOne({ _id: user._id });
+        res.json({ message: "User removed." });
+
+    } else {
+        res.status(404);
+        throw new Error("User not found.");
+    }
+})
+
 export {
     createUser,
     loginUser,
     logoutCurrentUser,
-    getAllUsers
+    getAllUsers,
+    getCurrentUserProfile,
+    updateCurrentUserProfile,
+    deleteUserById
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser, getAllUsers, loginUser, logoutCurrentUser } from "../controllers/userController.js";
+import { createUser, deleteUserById, getAllUsers, getCurrentUserProfile, loginUser, logoutCurrentUser, updateCurrentUserProfile } from "../controllers/userController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js"
 
 const router = express.Router();
@@ -8,5 +8,13 @@ router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers
 
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
+
+router.route("/profile")
+    .get(authenticate, getCurrentUserProfile)
+    .put(authenticate, updateCurrentUserProfile);
+
+// Admin Routes
+router.route("/:id").delete(authenticate, authorizeAdmin, deleteUserById);
+
 
 export default router;
